cde4332bb87a3e03510038fd30374d976977c800
AmirEphod <ephodamir@gmail.com>
2022-05-28 21:36:47 +0300

    hhh

--- static/map_js.js
+++ static/map_js.js
@@ -1,9 +1,4 @@
-// document.querySelector('#contact-form').addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     e.target.elements.name.value = '';
-//     e.target.elements.email.value = '';
-//     e.target.elements.message.value = '';
-//   });
+
 
 
 /*---------------- map*/
