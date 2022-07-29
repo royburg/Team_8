2eeb9ea8ef64dcfdeb0b715d2eadf9c7f237c965
Amit Moser <mosea@post.bgu.ac.il>
2022-07-29 15:22:53 +0300

    UPDATE

--- web_project_G8/settings.py
+++ web_project_G8/settings.py
@@ -3,13 +3,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Secret key setting from .env for Flask sessions
-SECRET_KEY = os.environ.get(b'\xc9\x8cY\xb1\x94yV\xb3\xc0n\xea\xa3\x0e\xbcOf'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # DB base configuration from .env for modularity and security reasons
 DB = {
-    'host' : os.environ.get('localhost'),
-    'user': os.environ.get('jdbc:mysql://localhost:3306'),
-    'password': os.environ.get('root1234'),
-    'database': os.environ.get('DB_G8')
+    'host' : os.environ.get('DB_HOST'),
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'database': os.environ.get('DB_NAME')
 }
